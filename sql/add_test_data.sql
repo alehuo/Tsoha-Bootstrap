7d069d76117c267468edc4a272de25e3d6533470
alehuo <aleksi.huotala@helsinki.fi>
2017-03-20 22:15:09 +0200

    Testi

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,7 +1,5 @@
---Laitokset
 INSERT INTO Laitos (nimi) VALUES ('Tietojenkäsittelytieteen laitos');
 INSERT INTO Laitos (nimi) VALUES ('Matematiikan ja tilastotieteen laitos');
---Kurssit
 INSERT INTO Kurssi (nimi, aloitusPvm, lopetusPvm, laitosId) VALUES('Ohjelmoinnin perusteet','2016-10-10','2017-01-10','1');
 INSERT INTO Kurssi (nimi, aloitusPvm, lopetusPvm, laitosId) VALUES('Ohjelmoinnin jatkokurssi','2017-01-11','2017-04-11','1');
 INSERT INTO Kurssi (nimi, aloitusPvm, lopetusPvm, laitosId) VALUES('Johdatus yliopistomatematiikkaan','2017-01-02','2017-04-02','2');
