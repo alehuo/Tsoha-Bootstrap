ea7c6da6a411e4442a462912fce057d42f66bf0c
Huotala Aleksi J H <ahuotala@da5-cs-b221-10.pc.helsinki.fi>
2017-03-22 18:22:13 +0200

    Lisätty käyttäjätilille salasana ja suola. Lisätty testidataa.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -4,5 +4,5 @@ INSERT INTO Kurssi (nimi, aloitusPvm, lopetusPvm, laitosId) VALUES('Ohjelmoinnin
 INSERT INTO Kurssi (nimi, aloitusPvm, lopetusPvm, laitosId) VALUES('Ohjelmoinnin jatkokurssi','2017-01-11','2017-04-11','1');
 INSERT INTO Kurssi (nimi, aloitusPvm, lopetusPvm, laitosId) VALUES('Johdatus yliopistomatematiikkaan','2017-01-02','2017-04-02','2');
 INSERT INTO Kurssi (nimi, aloitusPvm, lopetusPvm, laitosId) VALUES('Lineaarialgebra ja matriisilaskenta I','2017-01-02','2017-04-02','2');
-INSERT INTO Kayttaja (tyyppi,nimi) VALUES ('0','Olli Opiskelija');
-INSERT INTO Kayttaja (tyyppi,nimi) VALUES ('1','Pekka Pääkäyttäjä');
+INSERT INTO Kayttaja (tyyppi,nimi,salasana,suola) VALUES ('0','Olli Opiskelija','olli','asLK3k4230');
+INSERT INTO Kayttaja (tyyppi,nimi,salasana,suola) VALUES ('1','Pekka Pääkäyttäjä','admin','wrim4329a');
