0f6b11c64efcd913f1f47251af7cd289c64a1513
alehuo <aleksi.huotala@helsinki.fi>
2017-03-28 17:32:29 +0300

    Lisätty toiminnallisuutta kurssien selailuun, katseluun ja lisäämiseen

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,14 +1,6 @@
 $(document).ready(function () {
     //alert('Hello World!');
 });
-
-function luoInput(parentId, inputType, inputName, placeholder) {
-    var input = document.createElement('input');
-    input.type = inputType;
-    input.name = inputName;
-    input.placeholder = placeholder;
-    document.getElementById(parentId).appendChild(input);
-}
 function luoOpetusAikaKentta() {
     var element = $("#opAika").clone();
     element.removeAttr("id");
