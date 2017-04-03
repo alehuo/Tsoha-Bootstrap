1d9c144aa1522e368947464775d277616709937f
ahuotala <aleksi.huotala@helsinki.fi>
2017-04-03 16:46:17 +0300

    Korjaus

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -5,7 +5,7 @@ INSERT INTO Kurssi (kurssinimi, kuvaus, opintopisteet, aloitusPvm, lopetusPvm, v
 INSERT INTO Kurssi (kurssinimi, kuvaus, opintopisteet, aloitusPvm, lopetusPvm, vastuuYksikkoId) VALUES('Johdatus yliopistomatematiikkaan','Sisältää mm. joukko-opin ja kompleksiluvut.','5','1476057600','1486684800','2');
 INSERT INTO Kurssi (kurssinimi, kuvaus, opintopisteet, aloitusPvm, lopetusPvm, vastuuYksikkoId) VALUES('Lineaarialgebra ja matriisilaskenta I','Matriisien laskutoimitukset ja sovellukset','5','1476057600','1486684800','2');
 INSERT INTO Opetusaika (huone,viikonpaiva,aloitusAika,lopetusAika,kurssiId,tyyppi) VALUES('A111','0','795','840','1','0');
-INSERT INTO Kayttaja (tyyppi,nimi,salasana) VALUES ('1','Pekka Pääkäyttäjä','$2a$07$u83cCmViLjABIinOhXwWaOt6yyzxCSInw3qo5Q7lBXS3AkFeHuj3O');
+INSERT INTO Kayttaja (tyyppi,nimi,salasana) VALUES ('1','admin','$2a$07$u83cCmViLjABIinOhXwWaOt6yyzxCSInw3qo5Q7lBXS3AkFeHuj3O'); --admin::admin
 INSERT INTO KurssiIlmoittautuminen (kurssiId,kayttajaId) VALUES('1','1');
 INSERT INTO KurssiIlmoittautuminen (kurssiId,kayttajaId) VALUES('2','1');
 INSERT INTO Kurssisuoritus (kurssiId, kayttajaId, arvosana, paivays) VALUES('3','1','5','1541203200');
